531eb4d69fd076f03a518bc1946802b0f74adb9a
Prerit Garg <prerit2010@gmail.com>
2018-01-30 13:21:41 +0530

    predicting the sentence termination when token provided is '.' itself

--- classsifier.py
+++ classsifier.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import csv
 import pandas as pd
@@ -7,24 +6,25 @@ import nltk
 import re
 import pickle
 from nltk.tokenize import sent_tokenize, word_tokenize
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 dim = 25
 
-def loadGloveModel(gloveFile):
-    print("Loading Glove Model")
-    f = open(gloveFile,'r')
-    model = {}
-    for line in f:
-        splitLine = line.split()
-        word = splitLine[0]
-        embedding = np.array([float(val) for val in splitLine[1:]])
-        model[word] = embedding
-    print("Done.",len(model)," words loaded!")
-    return model
+# def loadGloveModel(gloveFile):
+#     print("Loading Glove Model")
+#     f = open(gloveFile,'r')
+#     model = {}
+#     for line in f:
+#         splitLine = line.split()
+#         word = splitLine[0]
+#         embedding = np.array([float(val) for val in splitLine[1:]])
+#         model[word] = embedding
+#     print("Done.",len(model)," words loaded!")
+#     return model
 
-def save_obj(obj, name ):
-    with open('obj/'+ name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+# def save_obj(obj, name ):
+#     with open('obj/'+ name + '.pkl', 'wb') as f:
+#         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name ):
     with open('obj/' + name + '.pkl', 'rb') as f:
@@ -34,34 +34,34 @@ def load_obj(name ):
 # # table = pd.read_table("27Bx25d.txt", sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
 # # nltk.download()
 
-word_vec = loadGloveModel("27Bx25d.txt")
+# word_vec = loadGloveModel("27Bx25d.txt")
 
-file = open("test.txt")
-text = file.read()
-token = word_tokenize(text)
-token = set(token)
-token = list(token)
-my_word_vec = {}
-count = 0
+# file = open("test.txt")
+# text = file.read()
+# token = word_tokenize(text)
+# token = set(token)
+# token = list(token)
+# my_word_vec = {}
+# count = 0
 
-for i, tok in enumerate(token):
-    re1 = re.search(r'(\w)+\W(\w)+', tok.lower())
-    re2 = re.search(r'(\w)+',tok.lower())
-    try: 
-        if re1:
-            my_word_vec[re1.group()] = word_vec[re1.group()]
-        elif re2:
-            my_word_vec[re2.group()] = word_vec[re2.group()]  
-    except:
-        rand_vec = np.random.rand(dim,)
-        if re1:
-            my_word_vec[re1.group()] = rand_vec
-        elif re2:
-            my_word_vec[re2.group()] = rand_vec
-        # print("token not found: ", tok)
-        count+=1
+# for i, tok in enumerate(token):
+#     re1 = re.search(r'(\w)+\W(\w)+', tok.lower())
+#     re2 = re.search(r'(\w)+',tok.lower())
+#     try: 
+#         if re1:
+#             my_word_vec[re1.group()] = word_vec[re1.group()]
+#         elif re2:
+#             my_word_vec[re2.group()] = word_vec[re2.group()]  
+#     except:
+#         rand_vec = np.random.rand(dim,)
+#         if re1:
+#             my_word_vec[re1.group()] = rand_vec
+#         elif re2:
+#             my_word_vec[re2.group()] = rand_vec
+#         # print("token not found: ", tok)
+#         count+=1
 
-save_obj(my_word_vec, "my_word_vec")
+# save_obj(my_word_vec, "my_word_vec")
 
 my_word_vec = load_obj("my_word_vec")
 # print(my_word_vec['blocked'])
@@ -71,12 +71,105 @@ my_word_vec = load_obj("my_word_vec")
 
 # print(count)
 
-print(my_word_vec)
+# print(my_word_vec)
 
 
 
+file=open("data/train.txt",'r')
+story=file.read()
+file.close()
+convo= re.findall(r'\"(.+?)\"',story,re.S)
+for i,str1 in enumerate(convo):
+    # if(story.find("\""+str1+"\"")) ==-1:
+    #     print("\""+str1+"\""+"NOT FOUND")
+
+    story=story.replace("\""+str1+"\"","id"+str(i))
+for i in range(len(convo)):
+    if(convo[i][-1]=="."):
+        convo[i]=convo[i][0:-1]+"@"
+    convo[i]=convo[i].replace(".","#")
+    convo[i]=convo[i].replace("@",".")
+i=len(convo)-1
+for str1 in reversed(convo):
+    story=story.replace("id"+str(i),"\""+str1+"\"")
+    i-=1
+# print(story)
+sentences=re.findall(r'<s>(.+?)</s>',story,re.S)
+tokens=[]
+for i in range(len(sentences)):
+    tokens.append(nltk.word_tokenize(sentences[i]))
+X=[]
+Y=[]
+#print(tokens)
+for i in range(len(tokens)):
+    for j in range((len(tokens[i]))):
+
+        if(tokens[i][j].find(".")!=-1 and j!=len(tokens[i])-1) and tokens[i][j+1]!="''":
+            X.append([i, j])
+            Y.append(0)
+        elif tokens[i][j].find(".")!=-1 :
+            X.append([i, j])
+            Y.append(1)
 
 
+X=np.array(X)
+Y=np.array(Y)
+# print(X,Y)
 
+# print(X.shape)
+# print(X)
 
+win_size = 10
+w = np.random.rand(dim,) #intialize model
+test = 90
 
+X_final = np.empty((0,dim))
+for indices in X:
+	total_vec = np.zeros(dim,)
+	count = 0
+	if tokens[indices[0]][indices[1]] == ".":
+		for i in range(win_size):
+			index_left = indices[1]-i-1
+			if index_left >= 0:
+				re1 = re.search(r'(\w)+\W(\w)+', tokens[indices[0]][index_left].lower())
+				re2 = re.search(r'(\w)+',tokens[indices[0]][index_left].lower())
+				if re1:
+					try:
+						vec = my_word_vec[re1.group()]
+						total_vec = np.add(total_vec, vec)
+						count+=1
+					except:
+						count+=0
+				elif re2:
+					try:
+						vec = my_word_vec[re2.group()]
+						total_vec = np.add(total_vec, vec)
+						count+=1
+					except:
+						count+=0
+			index_right = indices[1]+i+1
+
+			if index_right < len(tokens[indices[0]]):
+				re1 = re.search(r'(\w)+\W(\w)+', tokens[indices[0]][index_right].lower())
+				re2 = re.search(r'(\w)+',tokens[indices[0]][index_right].lower())
+				if re1:
+					vec = my_word_vec[re1.group()]
+					total_vec = np.add(total_vec, vec)
+					count+=1
+				elif re2:
+					vec = my_word_vec[re2.group()]
+					total_vec = np.add(total_vec, vec)
+					count+=1
+
+	if count != 0:
+		total_vec = np.divide(total_vec, count)
+
+	X_final = np.append(X_final, [total_vec], axis=0)
+
+Y_true = Y
+# print(X_final.shape, Y_final.shape)
+clf = svm.SVC()
+clf.fit(X_final[test:], Y_true[test:])
+Y_pred = clf.predict(X_final[:test])
+print(Y_true[:test], Y_pred)
+print(accuracy_score(Y_true[:test], Y_pred))
